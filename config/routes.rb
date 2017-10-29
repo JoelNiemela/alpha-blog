adf116c661602d90915e70b550b941d2fa7e154c
mocqoro <joel@niemela.se>
2017-10-29 21:34:08 +0000

    bootstrap styling on show page

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   get '/home', to: 'pages#home'
   get '/about', to: 'pages#about'
+  get '/new', to: 'articles#new'
   
   resources :articles
   # Example of regular route:
